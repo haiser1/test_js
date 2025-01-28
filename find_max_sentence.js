6c3a4417b468e159baf0fdc8e72fe662101131de
Mark <markusrr10@gmail.com>
2025-01-28 11:51:29 +0700

    first commit

---
+++ find_max_sentence.js
@@ -0,0 +1,18 @@
+const sentences = [
+    'Mauris ultricies sed sapien eget malesuada. Suspendisse et aliquet odio, id ultrices erat. Praesent vehicula erat nulla. Aliquam a lorem urna. Donec.',
+    'Duis at tellus et ex bibendum pellentesque sed in nibh. Sed aliquet, diam id mollis facilisis, massa metus accumsan elit, at mattis magna.',
+    'Sed non nibh quam. Pellentesque eget ultrices diam. Aliquam diam justo, consectetur ac dui lobortis, vestibulum bibendum lorem. Sed porta pulvinar.',
+  ];
+  
+  function result(sentences) {
+    let result = 0
+    for (let val of sentences){
+        let arr = val.split(" ")
+        if (arr.length > result){
+            result = arr.length
+        }
+    }
+    console.log(result)
+  }
+  
+  console.log(result(sentences));
